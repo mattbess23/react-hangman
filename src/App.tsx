a4d879e65389edaa378f54aa95bee402e5e17b9d
unknown <matthewbessick23@hotmail.com>
2024-03-30 22:12:18 +0200

    add first functionality

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,26 @@ function App() {
     return words[Math.floor(Math.random() * words.length)]
   })
 
-  console.log(wordToGuess)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
   
 
-  return <h1>Hi</h1>
-}
+  return ( 
+    <div 
+      style={{
+                maxWidth: "800px",
+                display: "flex",
+                flexDirection: "column",
+                gap:"2rem",
+                margin: "0 auto",
+                alignItems: "center"
+          }}
+        >
 
+    <div style={{ fontSize: "2rem", textAlign: "center"}}>
+      Lose 
+      Win
+    </div>
+  )
+}
 export default App
+ 
