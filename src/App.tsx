4f0a2426e30b8802786ea5f57f53140ed47381e7
unknown <matthewbessick23@hotmail.com>
2024-03-30 22:23:53 +0200

    edit Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
 import words from "./wordList.json"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
+
 
 function App() {
 
@@ -26,6 +30,10 @@ function App() {
       Lose 
       Win
     </div>
+    <HangmanDrawing/>
+    <HangmanWord />
+    <Keyboard />
+
   )
 }
 export default App
